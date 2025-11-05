be63b8df3358b0ee240389a75e9ada52c661ad56
Yogesh <yk5046013@gmail.com>
2025-11-05 21:30:30 +0530

    new change

--- programmes.c
+++ programmes.c
@@ -1,143 +1,148 @@
 #include <stdio.h>
+
+void rightaligntrianglewithnumbers();
 int main()
 {
-    printf("Hello world");
+    rightaligntrianglewithnumbers();
+
+    printf("Hello world\n");
+
     return 0;
 }
 
-// void age_validation()
-// {
-//     int age = 0;
-//     printf("Enter your age: ");
-//     scanf("%d", &age);
-//     if (age < 18)
-//     {
-//         printf("You are not eligible to vote");
-//     }
-//     else
-//     {
-//         printf("You are eligible to vote");
-//     }
-// }
+void age_validation()
+{
+    int age = 0;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    if (age < 18)
+    {
+        printf("You are not eligible to vote");
+    }
+    else
+    {
+        printf("You are eligible to vote");
+    }
+}
 
-// void postive_negative_zero()
-// {
-//     int num = 0;
-//     printf("Enter a number: ");
-//     scanf("%d", &num);
-//     if (num > 0)
-//     {
-//         printf("The number is positive");
-//     }
-//     else if (num < 0)
-//     {
-//         printf("The number is negative");
-//     }
-//     else
-//     {
-//         printf("The number is zero");
-//     }
-// }
+void postive_negative_zero()
+{
+    int num = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num > 0)
+    {
+        printf("The number is positive");
+    }
+    else if (num < 0)
+    {
+        printf("The number is negative");
+    }
+    else
+    {
+        printf("The number is zero");
+    }
+}
 
-// void odd_even()
-// {
-//     int num = 0;
-//     printf("Enter a number: ");
-//     scanf("%d", &num);
-//     if (num % 2 == 0)
-//     {
-//         printf("The number is even");
-//     }
-//     else
-//     {
-//         printf("The number is odd");
-//     }
-// }
+void odd_even()
+{
+    int num = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num % 2 == 0)
+    {
+        printf("The number is even");
+    }
+    else
+    {
+        printf("The number is odd");
+    }
+}
 
-// void leap_year()
-// {
-//     int year = 0;
-//     printf("Enter a year: ");
-//     scanf("%d", &year);
-//     if (year % 4 == 0)
-//     {
-//         printf("The year is a leap year");
-//     }
-//     else
-//     {
-//         printf("The year is not a leap year");
-//     }
-// }
+void leap_year()
+{
+    int year = 0;
+    printf("Enter a year: ");
+    scanf("%d", &year);
+    if (year % 4 == 0)
+    {
+        printf("The year is a leap year");
+    }
+    else
+    {
+        printf("The year is not a leap year");
+    }
+}
 
-// void alphabet_validation()
-// {
-//     char ch = 'a';
-//     printf("Enter a character: ");
-//     scanf("%c", &ch);
-//     if (ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z')
-//     {
-//         printf("The character is an alphabet");
-//     }
-//     else
-//     {
-//         printf("The character is not an alphabet");
-//     }
-// }
+void alphabet_validation()
+{
+    char ch = 'a';
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    if (ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z')
+    {
+        printf("The character is an alphabet");
+    }
+    else
+    {
+        printf("The character is not an alphabet");
+    }
+}
 
-// void divisibleby_5_and_17()
-// {
-//     int num = 0;
-//     printf("Enter a number: ");
-//     scanf("%d", &num);
-//     if (num % 5 == 0 && num % 17 == 0)
-//     {
-//         printf(
-//             "The number is divisble by 5 and 17");
-//     }
-//     else
-//     {
-//         printf(
-//             "The number is not divisble by 5 and 17");
-//     }
-// }
+void divisibleby_5_and_17()
+{
+    int num = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num % 5 == 0 && num % 17 == 0)
+    {
+        printf(
+            "The number is divisble by 5 and 17");
+    }
+    else
+    {
+        printf(
+            "The number is not divisble by 5 and 17");
+    }
+}
 
-// void for_loop()
-// {
-//     for (int i = 0; i < 10; i++)
-//     {
-//         printf("%d\n", i);
-//     }
-// }
+void for_loop()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\n", i);
+    }
+}
 
-// void while_loop()
-// {
-//     int i = 0;
-//     while (i < 10)
-//     {
-//         printf("%d\n", i);
-//         i++;
-//     }
-// }
+void while_loop()
+{
+    int i = 0;
+    while (i < 10)
+    {
+        printf("%d\n", i);
+        i++;
+    }
+}
 
-// void do_while_loop()
-// {
-//     int i = 0;
-//     do
-//     {
-//         printf("%d\n", i);
-//         i++;
-//     } while (i < 10);
-// }
+void do_while_loop()
+{
+    int i = 0;
+    do
+    {
+        printf("%d\n", i);
+        i++;
+    } while (i < 10);
+}
 
-// void infiniteloop()
-// {
-//     int i = 0;
-//     while (i >= 0)
-//     {
-//         printf("%d\n", i);
-//         i++;
-//     }
-// }
+void infiniteloop()
+{
+    int i = 0;
+    while (i >= 0)
+    {
+        printf("%d\n", i);
+        i++;
+    }
+}
 
 void degree_to_fahrenhiet()
 {
@@ -193,5 +198,100 @@ void sortlist()
     for (int i = 0; i < lengthofarray; i++)
     {
         printf("%d ", arr[i]);
+    }
+}
+
+void findoddinrange()
+{
+    int end = 0;
+    printf("Enter the end of the range: ");
+    scanf("%d", &end);
+    printf("Odd numbers in the range are: ");
+    while (end >= 0)
+    {
+        if (end % 2 != 0)
+        {
+            printf("%d ", end);
+        }
+        end--;
+    }
+}
+
+void multiplicationtable()
+{
+    int tablelength = 10;
+    int tablesize = 15;
+    printf("Enter the length of the table: ");
+    scanf("%d", &tablelength);
+    printf("Enter the table: ");
+    scanf("%d", &tablesize);
+    for (int i = 0; i < tablelength; i++)
+    {
+        for (int j = 0; j < tablesize; j++)
+        {
+            printf("%d * %d = %d", j, i + 1, j * (i + 1));
+        }
+        printf("\n");
+    }
+}
+
+void rightangletrianlewithstars()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+void rightaligntrianglewithstars()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (j >= 5 - i)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
+void rightangletrianlewithnumbers()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+void rightaligntrianglewithnumbers()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (j >= 5 - i)
+            {
+                printf("%d", i + 1);
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
     }
 }
